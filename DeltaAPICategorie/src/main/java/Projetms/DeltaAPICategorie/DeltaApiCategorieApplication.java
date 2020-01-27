e9e3e181ca684bcaa6501ea3b3a75c655812a63e
unknown <vp8.job@gmail.com>
2020-01-27 16:33:57 +0100

    Ajout du ms API categorie

---
+++ DeltaAPICategorie/src/main/java/Projetms/DeltaAPICategorie/DeltaApiCategorieApplication.java
@@ -0,0 +1,13 @@
+package Projetms.DeltaAPICategorie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaApiCategorieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaApiCategorieApplication.class, args);
+	}
+
+}
---
+++ DeltaAPICategorie/src/test/java/Projetms/DeltaAPICategorie/DeltaApiCategorieApplicationTests.java
@@ -0,0 +1,13 @@
+package Projetms.DeltaAPICategorie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaApiCategorieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
