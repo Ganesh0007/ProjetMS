a93fd4bdb3ee99010131193143890475943ba62c
unknown <vp8.job@gmail.com>
2020-01-27 16:31:05 +0100

    Ajout du ms API auteur

---
+++ DeltaAPIAuteur/src/main/java/Projetms/DeltaAPIAuteur/DeltaApiAuteurApplication.java
@@ -0,0 +1,13 @@
+package Projetms.DeltaAPIAuteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaApiAuteurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaApiAuteurApplication.class, args);
+	}
+
+}
---
+++ DeltaAPIAuteur/src/test/java/Projetms/DeltaAPIAuteur/DeltaApiAuteurApplicationTests.java
@@ -0,0 +1,13 @@
+package Projetms.DeltaAPIAuteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaApiAuteurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
