64a23e516c4c866dc500d7d0b9ab1f2b5383b0cf
unknown <vp8.job@gmail.com>
2020-01-27 16:32:56 +0100

    Ajout du ms API article

---
+++ DeltaAPIArticle/src/main/java/Projetms/DeltaAPIArticle/DeltaApiArticleApplication.java
@@ -0,0 +1,13 @@
+package Projetms.DeltaAPIArticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaApiArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaApiArticleApplication.class, args);
+	}
+
+}
---
+++ DeltaAPIArticle/src/test/java/Projetms/DeltaAPIArticle/DeltaApiArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package Projetms.DeltaAPIArticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaApiArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
